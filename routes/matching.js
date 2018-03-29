4c5fa9cb7c5cb87692b824438fc044e5be492331
Christopher Munroe <idkm23@gmail.com>
2018-03-29 02:42:44 -0400

    /event now uses ticketmaster api to load stuff

--- routes/matching.js
+++ routes/matching.js
@@ -1,8 +1,33 @@
+var http = require('http');
+var convertTime = require('convert-time');
+
 var utils = require('./utils');
+var tm = utils.tm;
 var models = require('../models');
 
 module.exports = function(app, passport) {
   app.get('/matching', utils.isLoggedIn, function(req, res) {
-    res.render('matching', {});
+    var options = {
+      host: tm.host,
+      path: tm.event_info_api + req.query.event_id + tm.api_key
+    };
+    utils.getJSON(options, function(status, tm_res) {
+      var time = convertTime(
+        tm_res.dates.start.localTime.slice(0, -3),
+        'HH:MM A'
+      );
+      var venue = tm_res._embedded.venues[0];
+      var location = venue.city.name + ", "
+        + (venue.state != null ? venue.state.name : venue.country.name);
+
+      res.render('matching', {
+        concert_title: tm_res.name,
+        venue: venue.name,
+        location: location,
+        date: tm_res.dates.start.localDate,
+        time: time,
+        tm_url: tm_res.url
+      });
+    });
   });
 };
--- routes/utils.js
+++ routes/utils.js
@@ -1,8 +1,48 @@
+var http = require("http");
+var https = require("https");
+
 module.exports = {
+  tm: {
+    api_key: "?apikey=etiKzCoqnYu3LmsKbArqF6uxdAJGaENS",
+    host: "app.ticketmaster.com",
+    event_info_api: "/discovery/v2/events/"
+  },
+
   isLoggedIn: function(req, res, next) {
     if (req.isAuthenticated())
       return next();
 
     res.redirect('/');
+  },
+
+  /**
+   * getJSON:  REST get request returning JSON object(s)
+   * @param options: http options object
+   * @param callback: callback to pass the results JSON object(s) back
+   */
+  getJSON: function(options, onResult) {
+    var port = options.port == 443 ? https : http;
+    var req = port.request(options, function(res)
+      {
+        var output = '';
+        res.setEncoding('utf8');
+
+        res.on('data', function (chunk) {
+          output += chunk;
+        });
+
+        res.on('end', function() {
+          var obj = JSON.parse(output);
+          console.log(obj);
+          onResult(res.statusCode, obj);
+        });
+      });
+
+    req.on('error', function(err) {
+      console.log('error: ' + err.message);
+      //res.send('error: ' + err.message);
+    });
+
+    req.end();
   }
 }
