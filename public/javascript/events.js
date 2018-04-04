4c0e6ef449e8295b05cdd88a5314e880f2ae7141
mbertucci1 <michael_bertucci@student.uml.edu>
2018-04-03 21:20:59 -0400

    Minor improvements to Events page
    Added loading icon to My Events panel while info is fetched. Fixed location field in search results table to show proper city/state info.

--- public/javascript/events.js
+++ public/javascript/events.js
@@ -1,12 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
+    
+    // Construct search results table
     $("#results-table").tabulator({
         layout:"fitColumns",
         height:"75%",
         placeholder:"Enter your search above",
         columns:[
             {title:"Event", field:"name", sorter:"string"},
-            {title:"Location", field:"dates.timezone", sorter:"string"},
-            {title:"Date", field:"dates.start.localDate", sorter:"date", sorterParams:{format:"YYYY-MM-DD"}},
+            {title:"Location", field:"location", sorter:"string"},
+            {title:"Date", field:"date", sorter:"date", sorterParams:{format:"YYYY-MM-DD"}},
             {title:"Id", field:"id", visible:false}
         ],
         rowClick:function(e, row){
@@ -18,6 +20,7 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
     
+    // Populate My Events panel
     $.get("/api/event/get_joined", function(data){
         console.log(data);
         setMyEvents(data);
@@ -43,9 +46,11 @@ function getSearchResults() {
         dataType: "json",
         success: function(json) {
             console.log(json);
-            // Parse the response.
-            // Do other things.
-            $("#results-table").tabulator("setData", json._embedded.events);
+            console.log("Res count: " + json.page.totalElements);
+            
+            if(json.page.totalElements > 0) {
+                $("#results-table").tabulator("setData", extractFields(json._embedded.events));
+            }
         },
         error: function(xhr, status, err) {
             console.log(err);
@@ -53,9 +58,32 @@ function getSearchResults() {
     });
 }
 
+/* Gets the relevant fields from API response to bind to table */
+function extractFields(eventArray) {
+    var tableData = {events:[]};
+    for(var i = 0; i < eventArray.length; i++) {
+        var rowData = {};
+        rowData['name'] = eventArray[i].name;
+        
+        var locationString = eventArray[i]._embedded.venues[0].city.name + ", ";
+        if(eventArray[i]._embedded.venues[0].state != null){
+            locationString += eventArray[i]._embedded.venues[0].state.stateCode;
+        } else {
+            locationString += eventArray[i]._embedded.venues[0].country.name;
+        }
+        rowData['location'] = locationString;
+        
+        rowData['date'] = eventArray[i].dates.start.localDate;
+        rowData['id'] = eventArray[i].id;
+        tableData.events.push(rowData);
+    }
+    
+    return tableData.events;
+}
+
 function setMyEvents(eventIdArray) {
     
-    var rootDiv = document.getElementById("my-events");
+    var rootDiv = document.getElementById("my-event-list");
     
     // If no events, append placeholder text div
     if(eventIdArray.length < 1) {
@@ -69,39 +97,44 @@ function setMyEvents(eventIdArray) {
         rootDiv.appendChild(item);
         
         return;
+        
+    } else {
+        
+        // Else create a div with the info on each event
+        var itemTemplate = "<a href=\"{{url}}\">" +
+                             "<h4>{{name}}</h4>" +
+                             "<p>{{date}}</p>" +
+                           "</a>";
+        eventIdArray.forEach(function(val, index) { 
+            var item = document.createElement("div");
+            item.className = "joined-event";
+            var itemContent = itemTemplate.replace(/{{url}}/g, "/matching?event_id=" + val);
+
+            var searchUrl = "https://app.ticketmaster.com/discovery/v2/events/" +
+                val +
+                ".json?&apikey=etiKzCoqnYu3LmsKbArqF6uxdAJGaENS";
+            $.ajax({
+                type:"GET",
+                url:searchUrl,
+                async:false,
+                dataType: "json",
+                success: function(json) {
+                    console.log(json);
+
+                    itemContent = itemContent.replace(/{{name}}/g, json.name);
+                    itemContent = itemContent.replace(/{{date}}/g, json.dates.start.localDate);
+                    item.innerHTML = itemContent;
+                    rootDiv.appendChild(item);
+                },
+                error: function(xhr, status, err) {
+                    console.log(err);
+                }
+            });
+        });
     }
     
-    // Else create a div with the info on each event
-    var itemTemplate = "<a href=\"{{url}}\">" +
-                         "<h4>{{name}}</h4>" +
-                         "<p>{{date}}</p>" +
-                       "</a>";
-    eventIdArray.forEach(function(val, index) { 
-        var item = document.createElement("div");
-        item.className = "joined-event";
-        var itemContent = itemTemplate.replace(/{{url}}/g, "/matching?event_id=" + val);
-        
-        var searchUrl = "https://app.ticketmaster.com/discovery/v2/events/" +
-            val +
-            ".json?&apikey=etiKzCoqnYu3LmsKbArqF6uxdAJGaENS";
-        $.ajax({
-            type:"GET",
-            url:searchUrl,
-            async:false,
-            dataType: "json",
-            success: function(json) {
-                console.log(json);
-                // Parse the response.
-                // Do other things.
-                itemContent = itemContent.replace(/{{name}}/g, json.name);
-                itemContent = itemContent.replace(/{{date}}/g, json.dates.start.localDate);
-                item.innerHTML = itemContent;
-                rootDiv.appendChild(item);
-            },
-            error: function(xhr, status, err) {
-                console.log(err);
-            }
-        });
-    });
+    // Remove loading icon and display div in its place
+    document.getElementById("loading-icon").style.display = "none";
+    rootDiv.style.display = "block";
 
 }
