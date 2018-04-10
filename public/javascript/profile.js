c46386ca8db13ee028b574e7f2fd18f86b0fdab2
mbertucci1 <michael_bertucci@student.uml.edu>
2018-04-10 06:02:04 -0400

    Implemented deleting profile pics

--- public/javascript/profile.js
+++ public/javascript/profile.js
@@ -1,8 +1,10 @@
 var imageContainer;
+var numPictures;  // Used to keep track of next available index when adding pics
 
 document.addEventListener('DOMContentLoaded', function () {
     
     imageContainer = document.getElementById("img-container");
+    numPictures = 0;
     
     $("#upload-button").click(function() {
         $("#file-input").val(null);
@@ -34,13 +36,14 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function addImageCard(imageData, rootElement) {
-    var template = "<img src={{data}} class=\"image\" height=\"250\">" +
-                   "<button class=\"remove-image-btn\">Delete</button>";
+    var template = "<img src={{data}} class=\"image\" height=\"250\" data-index=\"{{index}}\">" +
+                   "<button class=\"remove-image-btn\" onclick=\"deleteFile(event);\">Delete</button>";
     var item = document.createElement("div");
     item.className = "image-card";
-    item.innerHTML = template.replace(/{{data}}/g, imageData);
+    item.innerHTML = template.replace(/{{data}}/g, imageData).replace(/{{index}}/g, numPictures);
     
     rootElement.appendChild(item);
+    numPictures++;
 }
 
 /*  Take the byte array from the database and convert it back to a string
@@ -59,6 +62,10 @@ function _arrayBufferToBase64( buffer ) {
     //return window.btoa( binary );
 }
 
+/* Send base64 encoding of image file to server.
+ * @param fileName
+ *   The name of the image file from the file selection dialog
+ */
 function uploadFile(fileName) {
     if (window.File && window.FileReader) {
         var reader = new FileReader();
@@ -78,9 +85,42 @@ function uploadFile(fileName) {
                 }
             });
         };
-        reader.readAsDataURL(fileName);
+        reader.readAsDataURL(fileName);  // gets base64 string
 
     } else {
         alert("Browser does not support FileReader API");
+    }
+}
+
+/* Remove image from database.
+ * @param event
+ *   The click event containing the button that was clicked
+ */
+function deleteFile(event) {
+    var image = $(event.target).siblings('img');  // Get the img for that button
+    var imgIndex = image.attr('data-index');
+    console.log(imgIndex);
+    
+    $.ajax({
+        url:'/api/profile/delete_pic',
+        type:'POST',
+        data:{'index':imgIndex},
+        dataType:'json',
+        success: function(res) {
+            console.log("deletion response: ");
+            console.dir(res);
+            image.parent().remove();  // Remove whole card from list
+            refreshIndices();
+        }
+    });
+}
+
+function refreshIndices() {
+    var images = document.getElementsByClassName("image");
+    console.log("num images: " + images.length);
+    numPictures = 0;
+    for(var index = 0; index < images.length; index++) {
+        $(images[index]).attr('data-index', index);
+        numPictures++;
     }
 }
--- routes/profile.js
+++ routes/profile.js
@@ -22,6 +22,10 @@ module.exports = function(app, passport) {
     });
   });
     
+  /* ADD PROFILE PIC TO USER
+   * request body must contain 'img' field with 
+   * base64 string representation of picture
+   **/
   app.post('/api/profile/upload_pic', function(req, res) {
     var id = req.user.id;
     User.findById(id, {}).then(user => {
@@ -29,7 +33,6 @@ module.exports = function(app, passport) {
             user.pictures = [];
         }
         user.pictures.push(req.body.img);
-        console.log(req.body.img);
         user.update({
             pictures: user.pictures
         },{
@@ -40,6 +43,38 @@ module.exports = function(app, passport) {
             res.json(user);
         });
     });
+  });
+    
+  /* REMOVE PROFILE PIC FROM USER
+   * request body must contain 'index' field with 
+   * the index of the picture to remove from the array
+   **/
+  app.post('/api/profile/delete_pic', function(req, res) {
+      var id = req.user.id;
+      User.findById(id, {}).then(user => {
+          if(user.pictures) {
+              var index = req.body.index;
+              if(index >= 0 && index < user.pictures.length) {
+                  var removed = user.pictures.splice(index, 1);
+                  //console.log("Removed: " + removed);
+              }
+              user.update({
+                  pictures: user.pictures
+              }, {
+                  where: {
+                      id: req.user.id
+                  }
+              }).then(user => {
+                  res.json(user);
+              });
+          } else {
+              res.json({
+                  status: 1,
+                  message: 'Error: attempted to remove picture from empty array',
+                  req: req.body
+              });
+          }
+      });
   });
     
 };
